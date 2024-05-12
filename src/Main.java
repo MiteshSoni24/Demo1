adcd297315e3473d9bb05cbdab4d91abfc77ce7f
Mitesh <miteshsoni3255@gmail.com>
2024-05-12 13:49:38 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import Model.Employee;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //static
+        // Employee.display("Kavish","sr sd");
 
-             System.out.println("Hello and welcome!");
-
+        //nonstatic
+        Employee employee=new Employee();
+        employee.display("Kavita","Sr. rec");
         }
     }
